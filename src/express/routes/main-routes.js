f831f94f06cc85cb9e387d8d3ccf73d2ec297aa9
Климникова Мария Сергеевна <klimnikova_m@dalzaso.local>
2020-04-23 17:22:20 +1000

    Шаблоны

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -6,7 +6,9 @@ const mainRouter = new Router();
 mainRouter.get(`/`, (req, res) => res.render(`main`));
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
-mainRouter.get(`categories`, (req, res) => res.send(`/categories`));
+mainRouter.get(`/search`, (req, res) => res.render(`search`, {
+  admin: true
+}));
+mainRouter.get(`/categories`, (req, res) => res.render(`articles-by-category`));
 
 module.exports = mainRouter;
