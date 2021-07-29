794fe8d7d5a9f386e8b369adf1a25e426370a578
klimnikova <klimnikova@gmail.com>
2021-07-29 23:33:36 +0300

    add new model

---
+++ models/article-category.js
@@ -0,0 +1,17 @@
+'use strict';
+const {
+  Model
+} = require(`sequelize`);
+module.exports = (sequelize) => {
+  class ArticleCategory extends Model {
+    static associate() {
+
+    }
+  }
+  ArticleCategory.init({}, {
+    sequelize,
+    modelName: `articlesCategories`,
+    timestamps: false,
+  });
+  return ArticleCategory;
+};
