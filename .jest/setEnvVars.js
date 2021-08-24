e0121fcf0db635f1dcaa981f274a1c6d41ead915
klimnikova <klimnikova@gmail.com>
2021-08-24 20:53:44 +1000

    adds correct config for test env

---
+++ .jest/setEnvVars.js
@@ -0,0 +1,6 @@
+process.env.PUBLIC_URL='http://localhost'
+process.env.DB_HOST='localhost'
+process.env.DB_PORT=5432
+process.env.DB_NAME='database_test'
+process.env.DB_USER='postgres'
+process.env.DB_PASSWORD='academy'
