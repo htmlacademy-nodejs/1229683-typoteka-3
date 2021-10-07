a485ecd9e8b1bb3b1431f4857afb56081528a1a7
klimnikova <klimnikova@gmail.com>
2021-10-07 23:42:07 +0300

    improving other service consider user

--- src/service/data-service/search.js
+++ src/service/data-service/search.js
@@ -6,16 +6,26 @@ const {Op} = require(`sequelize`);
 class SearchService {
   constructor(sequelize) {
     this._Article = sequelize.models.article;
+    this._User = sequelize.models.user;
   }
 
   async findAll(searchText) {
+
     const articles = await this._Article.findAll({
       where: {
         title: {
           [Op.substring]: searchText
         }
       },
-      include: [`categories`],
+      include: [`categories`, {
+        model: this._User,
+        attributes: {
+          exclude: [`passwordHash`]
+        }
+      }],
+      order: [
+        [`createdAt`, `DESC`]
+      ]
     });
     return articles.map((article) => article.get());
   }
